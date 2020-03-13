7802afbaa89e04b837695d3106b33b6242a50151
Elbert Ballo <eballofullstackdev@gmail.com>
2020-03-13 13:25:10 -0700

    Updates

---
+++ precache-manifest.faeaaa65dbea3bfa86d10321f55e770e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703ee5bc87f1e69ecc6b3b65affc5bb1",
+    "url": "/yelp-api-project/index.html"
+  },
+  {
+    "revision": "dff3818b05d9f2cc6aa6",
+    "url": "/yelp-api-project/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "094bef5fbb98f6ec7f2f",
+    "url": "/yelp-api-project/static/css/main.db0b0e47.chunk.css"
+  },
+  {
+    "revision": "dff3818b05d9f2cc6aa6",
+    "url": "/yelp-api-project/static/js/2.1a98fb4a.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/yelp-api-project/static/js/2.1a98fb4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "094bef5fbb98f6ec7f2f",
+    "url": "/yelp-api-project/static/js/main.3919fdda.chunk.js"
+  },
+  {
+    "revision": "83d6ab6cdb3f4b98a29a",
+    "url": "/yelp-api-project/static/js/runtime-main.194c928f.js"
+  },
+  {
+    "revision": "9aa0a590516b1dd0f71c89c10b2cd4c9",
+    "url": "/yelp-api-project/static/media/food-image.9aa0a590.png"
+  }
+]);
